bdbe81a48c72a08489f79a04d7530fa1d09a57c1
Philip Abbet <philip.abbet@idiap.ch>
2024-12-07 10:58:33 +0100

    Update astrometry.net-lite again

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -49,7 +49,7 @@ set(ASTROMETRY_NET_LITE_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     astrometry-net-lite
     GIT_REPOSITORY https://github.com/Kanma/astrometry.net-lite.git
-    GIT_TAG "09f96d16127bab127dc65176fcef60dcebbf0550"
+    GIT_TAG "e4e6f24316db83392ff64a23ff193af3b2704b90"
 )
 
 FetchContent_MakeAvailable(astrometry-net-lite)
