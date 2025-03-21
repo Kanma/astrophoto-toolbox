c212bb34cefc54b1b22721c40ef820f1e6c8b4dc
Philip Abbet <philip.abbet@gmail.com>
2025-03-21 17:53:55 +0100

    The 'unittests' executable is now in charge of cleaning its temp folder

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,5 +6,21 @@
  * SPDX-License-Identifier: BSD-3-Clause
 */
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include <filesystem>
+
+
+int main(int argc, char** argv)
+{
+    // Ensures the temp folders used during the tests are empty
+    std::filesystem::remove_all(TEMP_DIR);
+    std::filesystem::create_directory(TEMP_DIR);
+    std::filesystem::create_directory(TEMP_DIR "/empty");
+
+    // Executes the tests
+    int result = Catch::Session().run(argc, argv);
+
+    return result;
+}
